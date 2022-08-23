994b3b86b2c0eccfa2fdfb5b45ebc4f5e545d6f2
Lucas <Rocchiettilucas9@gmail.com>
2022-08-22 23:33:19 -0300

    JSX

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,25 @@
+const Header = () => {
+    return (
+        <header>
+        <nav>
+            <ul>
+                <li>
+                    <li>
+                    <a href='https://google.com'>Productos</a>
+                    </li>
+                    <li>
+                    <a href='https://google.com'>¿Quiénes Somos?</a>
+                    </li>
+                    <li>
+                    <a href='https://google.com'>Contacto</a>      
+                    </li>
+                    <li>
+                    <a href='https://google.com'>Mi cuenta</a>
+                    </li>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    );
+};
+export default Header;
